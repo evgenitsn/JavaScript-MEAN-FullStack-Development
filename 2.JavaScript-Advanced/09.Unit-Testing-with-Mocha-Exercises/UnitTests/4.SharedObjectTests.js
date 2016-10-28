21bbb17d7bc01921762e48f70e2933ba2828df72
evgeni-tsn <evgeni.tsv.nikolov@gmail.com>
2016-10-28 19:21:29 +0300

    [JS] Unit Testing with Mocha & Chai + Classes Lab

--- 2.JavaScript-Advanced/09.Unit-Testing-with-Mocha-Exercises/UnitTests/4.SharedObjectTests.js
+++ 2.JavaScript-Advanced/09.Unit-Testing-with-Mocha-Exercises/UnitTests/4.SharedObjectTests.js
@@ -4,31 +4,154 @@
 let sharedObject = require('../4.SharedObject').sharedObject
 let expect = require('chai').expect
 
-// describe('sharedObject', function() {
-//   describe('name and income', function() {
-//     it('', function () {
-//       expect(sharedObject.name).to.be.null
-//     })
-//     it('', function () {
-//       expect(sharedObject.name = 'google').to.be.equal('google')
-//     })
-//     //obj --> undef?
-//     it('', function () {
-//       expect(sharedObject.income).to.be.null
-//     })
-//     it('', function () {
-//       expect(sharedObject.income = 12).to.be.equal(12)
-//     })
-//     // it('', function() {
-//     //   expect(sharedObject.income='nnn').to.be.undefined
-//     // })
-//   })
-//   describe('name and income', function() {
-//     it('', function () {
-//       let name = sharedObject.name
-//       sharedObject.changeName('Hello')
-//       let newName = 'Hello'
-//       expect(name).to.be.equal('Hello')
-//     })
-//   })
-// })
+describe('shared object test', function () {
+  it('', function() {
+    sharedObject.changeName('kolev')
+
+    expect($('#name').val()).to.be.equal('kolev')
+    expect(sharedObject.name).to.be.equal('kolev')
+  })
+
+  it('', function() {
+    let valueBefore = $('#name').val()
+    let propBefore = sharedObject.name
+    sharedObject.changeName('')
+
+
+    expect($('#name').val()).to.be.equal(valueBefore)
+    expect(sharedObject.name).to.be.equal(propBefore)
+  })
+
+  it('', function() {
+    let valueBefore = $('#name').val()
+    let propBefore = sharedObject.name
+    sharedObject.changeName(4)
+
+
+    expect($('#name').val()).to.be.equal(valueBefore)
+    expect(sharedObject.name).to.be.equal(propBefore)
+  })
+
+  it('', function() {
+    sharedObject.changeIncome(550)
+
+    expect($('#income').val()).to.be.equal('550')
+    expect(sharedObject.income).to.be.equal(550)
+  })
+
+  it('', function() {
+    let beforeVal = $('#income').val()
+    let beforeProp = sharedObject.income
+    sharedObject.changeIncome(-500)
+
+    expect($('#income').val()).to.be.equal(beforeVal)
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let smth = sharedObject.income = 100
+    sharedObject.changeIncome(0)
+    expect(sharedObject.income).to.be.equal(100)
+  })
+
+  it('', function() {
+    let beforeVal = $('#income').val()
+    let beforeProp = sharedObject.income
+    sharedObject.changeIncome(-50.3)
+
+    expect($('#income').val()).to.be.equal(beforeVal)
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let beforeVal = $('#income').val()
+    let beforeProp = sharedObject.income
+    sharedObject.changeIncome(5.4)
+
+    expect($('#income').val()).to.be.equal(beforeVal)
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let beforeVal = $('#income').val()
+    let beforeProp = sharedObject.income
+    sharedObject.changeIncome('500')
+
+    expect($('#income').val()).to.be.equal(beforeVal)
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let propBefore = sharedObject.name
+    $('#name').val('')
+
+    sharedObject.updateName()
+    expect(sharedObject.name).to.be.equal(propBefore)
+
+  })
+
+  it('', function() {
+    let propBefore = sharedObject.name
+    $('#name').val(undefined)
+
+    sharedObject.updateName()
+    expect(sharedObject.name).to.be.equal(propBefore)
+  })
+
+  it('', function() {
+    let propBefore = sharedObject.name
+    $('#name').val(null)
+
+    sharedObject.updateName()
+    expect(sharedObject.name).to.be.equal(propBefore)
+  })
+
+  it('', function() {
+    let propBefore = sharedObject.name
+    $('#name').val(5)
+
+    sharedObject.updateName()
+    expect(sharedObject.name).to.be.equal(propBefore)
+  })
+
+  it('', function() {
+
+    $('#name').val('kolev')
+
+    sharedObject.updateName()
+    expect(sharedObject.name).to.be.equal('kolev')
+  })
+
+  it('', function() {
+    let beforeProp = sharedObject.income
+    $('#income').val(NaN)
+
+    sharedObject.updateIncome()
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let beforeProp = sharedObject.income
+    $('#income').val(5)
+
+    sharedObject.updateIncome()
+    expect(sharedObject.income).to.be.equal(5)
+  })
+
+  it('', function() {
+    let beforeProp = sharedObject.income
+    $('#income').val(-5)
+
+    sharedObject.updateIncome()
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+
+  it('', function() {
+    let beforeProp = sharedObject.income
+    $('#income').val(undefined)
+
+    sharedObject.updateIncome()
+    expect(sharedObject.income).to.be.equal(beforeProp)
+  })
+})
+
